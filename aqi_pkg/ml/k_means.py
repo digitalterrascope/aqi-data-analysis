c56fd1bf89ddb54d5fa3d73bdd7387212c88173f
digitalterrascope <xxxxdolphin@gmail.com>
2026-02-17 06:20:19 +0530

    Perform outliner detection and k-means

--- aqi_pkg/ml/k_means.py
+++ aqi_pkg/ml/k_means.py
@@ -3,11 +3,13 @@ from aqi_pkg.filters import *
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+from sqlalchemy import select
+import pandas as pd
 
 measurements = ["AQI_IN", "AQI_US", "CO_PPB", "NO2_PPB", "O3_PPB", "SO2_PPB", "PM1_UGM3", "PM2_5_UGM3", "PM10_UGM3", "H_PERCENT", "T_C", "TVOC_PPM", "Noise_DB"]
 
 def k_means_clustering(session, K=range(2, 20)):
-    data = get_all_measurements(session, measurements, city="Chandigarh". start=datetime(2025, 12, 1), end=datetime(2025, 12, 7))
+    data = get_all_measurements(session, measurements, city="Chandigarh")
     data = np.array(data)
     print(data.shape)
 
@@ -29,17 +31,66 @@ def k_means_clustering(session, K=range(2, 20)):
 
     inertias = []
     for k in K:
-        start_time = time.time()
         kmeans = KMeans(n_clusters=k, random_state=42)
         kmeans.fit(data)
         inertias.append(kmeans.inertia_)
 
         print(f"Fitted KMeans with k={k}, inertia={kmeans.inertia_:.2f}")
-        print(f"Time taken: {time.time() - start_time:.2f} seconds")
-
 
     return K, inertias
 
+
+def k_means_spatial_clustering(session, K=6):
+    stmt = (
+        select(
+            Entry.locationId,
+            Entry.last_updated,
+            Entry.AQI_IN,
+            Entry.lat,
+            Entry.lon
+        )
+        .where(
+            Entry.AQI_IN != None,
+            Entry.city.in_([
+                'Chandigarh',
+                'Mohali',
+                'Dera Bassi',
+                'Sahibzada Ajit Singh Nagar',
+                'Zirakpur',
+                'Panchkula'
+            ])
+        )
+    )
+
+    result = session.execute(stmt)
+
+    df = pd.DataFrame(result.mappings().all())
+    df['time'] = pd.to_datetime(df["last_updated"])
+
+    target_date = '2-10-2026'
+    if isinstance(target_date, str):
+        target_date = pd.to_datetime(target_date).date()
+    df = df[df['time'].dt.date == target_date]
+
+    df_avg = df.groupby(['locationId', 'lat', 'lon'], as_index=False)['AQI_IN'].mean()
+
+    coords = df_avg[['lat', 'lon', 'AQI_IN']].values
+    coords = (coords - np.mean(coords, axis=0)) / np.std(coords, axis=0)
+    kmeans = KMeans(n_clusters=K, random_state=42)
+    df_avg['cluster'] = kmeans.fit_predict(coords)
+
+    plt.figure(figsize=(8, 6))
+    for cluster in range(K):
+        cluster_data = df_avg[df_avg['cluster'] == cluster]
+        plt.scatter(
+            cluster_data['lon'], cluster_data['lat'],
+            label=f'Cluster {cluster}', s=50
+        )
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title(f'KMeans Spatial Clustering ({K} clusters) - {target_date}')
+    plt.legend()
+    plt.savefig('test_img/k_means_cluster')
 
 
 def plot_inertia(K, inertias, filename=None):
--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from datetime import datetime
 from aqi_pkg.db import get_session
 from aqi_pkg.filters import *
 from aqi_pkg.plots import *
-from aqi_pkg.ml.k_means import k_means_clustering, plot_inertia
+from aqi_pkg.ml.k_means import *
+import folium
+from folium.plugins import HeatMap
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from io import BytesIO
+import base64
+# from aqi_pkg.ml.k_means import k_means_clustering, plot_inertia
 
 measurements = ["AQI_IN", "AQI_US", "CO_PPB", "NO2_PPB", "O3_PPB", "SO2_PPB", "PM1_UGM3", "PM2_5_UGM3", "PM10_UGM3", "H_PERCENT", "T_C", "TVOC_PPM", "Noise_DB"]
 
-
 def main(session):
-    pass
-
-
+    k_means_spatial_clustering(session, 8)
 
 if __name__ == "__main__":
     session = get_session()
